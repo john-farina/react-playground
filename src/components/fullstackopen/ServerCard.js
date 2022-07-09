185857ddef38c1356c4b38526e82115fa0024c6e
onefootfarina <clichecontent@gmail.com>
2022-07-08 21:29:21 -0400

    search!!!!!

--- src/components/fullstackopen/ServerCard.js
+++ src/components/fullstackopen/ServerCard.js
@@ -1,14 +1,34 @@
+import { useState } from 'react';
+
 function ServerCard({ list }) {
-    console.log(list);
+    // console.log(list);
     const styles = {
         overflowY: 'scroll',
     };
+    const result = list.filter((word) => word.name.common.includes('y'));
+    // console.log(result);
+    const [search, setSearch] = useState([]);
+
+    function handleSearchChange(event) {
+        console.log(event.target.value);
+        const results = list.filter((names) =>
+            names.name.common.includes(event.target.value)
+        );
+        setSearch(results);
+        console.log(results);
+    }
+
     return (
         <>
             <div style={styles} className="card">
+                <input
+                    onChange={handleSearchChange}
+                    type="text"
+                    placeholder="search"
+                />
                 <h2>i got this from a server wow</h2>
-                {list.map((item) => {
-                    return <p>{item.name.common}</p>;
+                {search.map((item) => {
+                    return <p key={item.name.common}>{item.name.common}</p>;
                 })}
             </div>
         </>
